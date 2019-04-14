675d00dd2c0cb3abcd9c6c5d611ddc5dcc183402
Kemal YILDIRIM <kemal_yldrim@outlook.com>
2019-04-14 22:17:21 +0100

    BezierIO form created, group box added to form

--- Bezier.WinForm/Forms/BezierIO.Designer.cs
+++ Bezier.WinForm/Forms/BezierIO.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Bezier.WinForm
+﻿namespace Bezier.WinForm.Forms
 {
-    partial class Form1
+    partial class BezierIO
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(602, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 526);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // BezierIO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(878, 582);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BezierIO";
+            this.Text = "BezierIO";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
-
--- Bezier.WinForm/Forms/BezierIO.cs
+++ Bezier.WinForm/Forms/BezierIO.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Bezier.WinForm
+namespace Bezier.WinForm.Forms
 {
-    public partial class Form1 : Form
+    public partial class BezierIO : Form
     {
-        public Form1()
+        public BezierIO()
         {
             InitializeComponent();
         }
